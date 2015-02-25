b47b6f08f935e058e01f6de1eafd2e88c6fd64c8
Edward <exiphia@hotmail.com>
2015-02-25 17:21:54 +0000

    Added some of the core files.
    WARNING: Not finished, try and let me finish it later.
    NOTE: Could someone make some menu art. The dimensions are in
    "GamePanel" (1280, 720).
    Thanks
    - Edward

---
+++ Main/Game.java
@@ -0,0 +1,17 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class Game {
+	
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame("Delta Deathmatch");
+		window.setContentPane(new GamePanel());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.pack();
+		window.setVisible(true);	
+	}
+
+}
